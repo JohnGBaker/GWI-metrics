bc974d0917be3e97a2f5bb77a67726ec1d4084f8
jslutsky <jacob.p.slutsky@nasa.gov>
2022-09-12 13:51:42 -0400

    added GOBIG(ALIA)

--- src/concepts.py
+++ src/concepts.py
@@ -218,6 +218,19 @@ ALIAlowL = {
     'TMsize' : .046*2.5 # .046
 }
 
+#Baseline LISA concept GoBIGLowF
+GoBIGALIA = {
+    'label' : 'GoBig(ALIA)',
+    'description' : 'Two ALIA-like constellations on near-radial trajectories (in near opposing directions) to larger distance in solar system. This version uses a LISA-sized constellation',
+    'label' : 'GOBIGALIA',
+    'sqSacc_ASD' : [[6e-16,.4e-3*6e-16],[0,-1]],
+    'sqSoms_ASD' : [[5e-13,5e-13*4.e-6],[0,-2]],
+    'Lconst' : 0.5e9,
+    'Dsep' : 30,
+    'Nindep' : 4,
+    'SciDuration' : 4
+}
+
 #Baseline DECIGO
 DECIGO = {
     'label' : 'DECIGO',
